dec663be04ce5bbec87da32d8693521692594228
Shreyash Kumar <76476889+Shreyk919@users.noreply.github.com>
2021-03-09 21:08:47 +0530

    Update main class to check more conditions.

--- src/main/scala/org/knoldus/implementations/main_class.scala
+++ src/main/scala/org/knoldus/implementations/main_class.scala
@@ -15,18 +15,22 @@ import traits.person
 
              val person1 = new person("John", 42)      //input person1
 
-             val person2 = new person("Marcus", 23)            //input person2
+             val person2 = new person("Marcus", 23)    //input person2
+
+             val person3 = new person("David", 34)     //input person3
+
+             val person4 = new person("David", 16)     //input person4
 
              /*
-                checking all possible comparison options between person1 and person2
+                checking all possible comparison options between 4 inputs
               */
-             println(person1 < person2)
-
-             println(person2 > person1)
-
              println(person1 >= person2)
 
              println(person2 >= person1)
+
+             println(person3 < person4)
+
+             println(person4 < person3)
 
         }
     }
